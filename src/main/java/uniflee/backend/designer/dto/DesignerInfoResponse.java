f8a12927540199921ded4492eacdb7eb193bcb22
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2024-09-26 20:16:56 +0900

    Refactor: DesignerController - #13 - @Operation, @Schema 추가

--- src/main/java/uniflee/backend/designer/dto/DesignerInfoResponse.java
+++ src/main/java/uniflee/backend/designer/dto/DesignerInfoResponse.java
@@ -2,6 +2,7 @@ package uniflee.backend.designer.dto;
 
 import static lombok.AccessLevel.*;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,8 +12,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
+@Schema(description = "디자이너 정보에 대한 응답 데이터 객체")
 public class DesignerInfoResponse {
+	@Schema(description = "디자이너의 이름", example = "홍길동")
 	private String name;
+	@Schema(description = "디자이너의 프로필 이미지 URL", example = "https://example.com/profile.jpg")
 	private String profileImageUrl;
+	@Schema(description = "디자이너의 배경 이미지 URL", example = "https://example.com/background.jpg")
 	private String backgroundImageUrl;
 }
