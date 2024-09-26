ddcd0c1ce71f903e97a7beae2971530b63dc36cb
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2024-09-26 12:42:59 +0900

    Feat: JWT 기능 개발 - #9 - jwt 인증, 토큰 검증을 위한 jwtFilter 추가 - jwt 예외처리를 위한 JwtExceptionHandler, AuthenticationEntryPoint 추가 - jwt filter와 예외처리를 적용하기 위한 Security Config 수정

--- src/main/java/uniflee/backend/Security/SecurityConfig.java
+++ src/main/java/uniflee/backend/Security/SecurityConfig.java
@@ -50,10 +50,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/user").permitAll()
-                .anyRequest().denyAll()
+                .anyRequest().permitAll()
+
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
 
                 // 로그인 필터
                 .and()
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .addFilterAt(
                         new LoginFilter(
                                 authenticationManager(authenticationConfiguration), jwtProvider), UsernamePasswordAuthenticationFilter.class);
